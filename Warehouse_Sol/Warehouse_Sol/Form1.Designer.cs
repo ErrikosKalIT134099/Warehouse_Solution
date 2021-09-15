1d9ce231d8e8d9dbcdbc011e0b7ffa08288b0d3e
ErrikosKalIT134099 <44244924+ErrikosKalIT134099@users.noreply.github.com>
2021-09-15 21:35:39 +0300

    added project ui

--- Warehouse_Sol/Warehouse_Sol/Form1.Designer.cs
+++ Warehouse_Sol/Warehouse_Sol/Form1.Designer.cs
@@ -36,6 +36,14 @@ namespace Warehouse_Sol
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.headTB = new System.Windows.Forms.TextBox();
+            this.melosTB = new System.Windows.Forms.TextBox();
+            this.melos2TB = new System.Windows.Forms.TextBox();
+            this.changeDetBTN = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // titleLB
@@ -88,9 +96,9 @@ namespace Warehouse_Sol
             this.label3.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(15, 266);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(163, 17);
+            this.label3.Size = new System.Drawing.Size(83, 17);
             this.label3.TabIndex = 5;
-            this.label3.Text = "Ημερομηνία Απογραφής: ";
+            this.label3.Text = "Διευθυντής: ";
             // 
             // label4
             // 
@@ -98,15 +106,79 @@ namespace Warehouse_Sol
             this.label4.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(15, 343);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(163, 17);
+            this.label4.Size = new System.Drawing.Size(87, 17);
             this.label4.TabIndex = 6;
-            this.label4.Text = "Ημερομηνία Απογραφής: ";
+            this.label4.Text = "Λοιπά Μέλη:";
+            // 
+            // headTB
+            // 
+            this.headTB.Location = new System.Drawing.Point(18, 286);
+            this.headTB.Name = "headTB";
+            this.headTB.Size = new System.Drawing.Size(160, 20);
+            this.headTB.TabIndex = 7;
+            // 
+            // melosTB
+            // 
+            this.melosTB.Location = new System.Drawing.Point(18, 363);
+            this.melosTB.Name = "melosTB";
+            this.melosTB.Size = new System.Drawing.Size(160, 20);
+            this.melosTB.TabIndex = 8;
+            // 
+            // melos2TB
+            // 
+            this.melos2TB.Location = new System.Drawing.Point(18, 399);
+            this.melos2TB.Name = "melos2TB";
+            this.melos2TB.Size = new System.Drawing.Size(160, 20);
+            this.melos2TB.TabIndex = 9;
+            // 
+            // changeDetBTN
+            // 
+            this.changeDetBTN.Location = new System.Drawing.Point(190, 245);
+            this.changeDetBTN.Name = "changeDetBTN";
+            this.changeDetBTN.Size = new System.Drawing.Size(108, 23);
+            this.changeDetBTN.TabIndex = 10;
+            this.changeDetBTN.Text = "Αλλαγή Στοιχείων";
+            this.changeDetBTN.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(716, 456);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Διαγραφή \'Ολων";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(363, 716);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 49);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Προεπισκόπηση";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(40, 485);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(802, 224);
+            this.dataGridView1.TabIndex = 12;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(884, 777);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.changeDetBTN);
+            this.Controls.Add(this.melos2TB);
+            this.Controls.Add(this.melosTB);
+            this.Controls.Add(this.headTB);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox1);
@@ -116,6 +188,7 @@ namespace Warehouse_Sol
             this.Controls.Add(this.titleLB);
             this.Name = "Form1";
             this.Text = " Απογραφή Προιόντων Αποθήκης";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -130,6 +203,13 @@ namespace Warehouse_Sol
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox headTB;
+        private System.Windows.Forms.TextBox melosTB;
+        private System.Windows.Forms.TextBox melos2TB;
+        private System.Windows.Forms.Button changeDetBTN;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
